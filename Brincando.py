b888abce7c8abc8ba7b2f956c0596829d3e24859
augustofareis <augustofareis@gmail.com>
2024-12-15 16:26:12 -0300

    Update Brincando.py

--- Brincando.py
+++ Brincando.py
@@ -1,6 +1,7 @@
 #import warnings
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from filtros import *  # Importando a função
